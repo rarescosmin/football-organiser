74cd41a14a38cfb296450061f5d51a1a6b9f21d4
rarescosmin <rarescosmin97@gmail.com>
2021-09-13 19:41:10 +0300

    Added material ui

---
+++ components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import styles from './Layout.module.css';
+
+const Layout = ({ children }) => {
+    return (
+        <div className={styles.container}>
+            <main className={styles.main}>
+                { children }
+            </main>
+        </div>
+    )
+}
+
+export default Layout
